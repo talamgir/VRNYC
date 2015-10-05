993f813094d3ceaf121e62580107bae9da79f347
Taimur Alamgir <talamgir@law.gwu.edu>
2015-10-05 14:12:37 -0400

    Confirmation-Page

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require "sinatra"
 require "mandrill"
 
-m = Mandrill::API.new
+enable :sessions
 
 get "/home" do
-	erb :contactus 
+  erb :contactus
 end
 
 post '/confirm' do 
@@ -14,15 +14,36 @@ post '/confirm' do
     message = {
               :subject=> "Reservation Confirmation for VRNYC",
               :to=> [{
-                      :email=> "talamgir@law.gwu.edu",
+                      :email=> "#{params[:user_email]}",
                       :name=> "VRNYC Experience",
                     }],
               :from_email=> "talamgir@law.gwu.edu",
-              :html=> "<html>#{params[:user_name]}," + "you are confirmed for your reservation, your comments were " + "#{params[:user_message]}</html>"
-              # :message=> "#{params[:user_name]}," + "you are confirmed for your reservation, your comments were " + "#{params[:user_message]}"
+              :html=> "<html>#{params[:user_input]}," + "you are confirmed for your reservation, your comments were " + "#{params[:user_story]}</html>"
+              # :message=> "#{params[:user_name]}," + "you are confirmed for your reservation, your comments were " + "#{params[:user_story]}"
             }
             
-  sending = m.messages.send(message)
+  sending = mandrill.messages.send(message)
   puts sending
-  redirect to('/confirm')
+
+  ## Redirect
+  user_input = params[:user_input]
+  session[ :message_name] = "#{user_input}"
+
+  user_email = params[:user_email]
+  session[ :message_email] = "#{user_email}"
+
+  user_story = params[:user_story]
+  session[ :message_story] = "#{user_story}"
+
+  redirect "/confirmation"
+end
+
+get '/confirmation' do
+
+  @message_name = session.delete(:message_name)
+  @message_email = session.delete(:message_email)
+  @message_story = session.delete(:message_story)
+  erb :confirmation
+
+  # :locals => {:user_input => params[:user_input], :user_email=> params[:user_email], :user_story => params[:user_story]}
 end
